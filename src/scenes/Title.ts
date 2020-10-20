9201612d00a306c9d177477d1059e680ed870ea9
asmith60 <asmith60@gmail.com>
2020-10-20 15:45:43 -0500

    remove loading screen

--- src/scenes/Title.ts
+++ src/scenes/Title.ts
@@ -12,54 +12,6 @@ export default class Yard extends phaser.Scene {
     }
 
     preload() {
-        const progressBar = this.add.graphics();
-        const progressBox = this.add.graphics();
-        progressBox.fillStyle(0x222222, 0.8);
-        progressBox.fillRect(240, 270, 320, 50);
-
-        const width = this.cameras.main.width;
-        const height = this.cameras.main.height;
-        const loadingText = this.make.text({
-            x: width / 2,
-            y: height / 2 - 50,
-            text: 'Loading...',
-            style: {
-                font: '20px monospace',
-                fill: '#ffffff'
-            }
-        });
-        loadingText.setOrigin(0.5, 0.5);
-
-        const percentText = this.make.text({
-            x: width / 2,
-            y: height / 2 - 5,
-            text: '0%',
-            style: {
-                font: '18px monospace',
-                fill: '#ffffff'
-            }
-        });
-        percentText.setOrigin(0.5, 0.5);
-
-        this.load.on('progress', (value: any) => {
-            console.log(value);
-            percentText.setText(parseInt((value * 100).toString()) + '%');
-            progressBar.clear();
-            progressBar.fillStyle(0xffffff, 1);
-            progressBar.fillRect(250, 280, 300 * value, 30);
-        });
-
-        // this.load.on('fileprogress', (file: any) => {
-        //     console.log(file.src);
-        // });
-
-        this.load.on('complete', () => {
-            progressBar.destroy();
-            progressBox.destroy();
-            loadingText.destroy();
-            percentText.destroy();
-        });
-
         this.load.image('cars', 'assets/tilesets/cars.png');
         this.load.image('chairs', 'assets/tilesets/chairs.png');
         this.load.image('farm', 'assets/tilesets/farm.png');
